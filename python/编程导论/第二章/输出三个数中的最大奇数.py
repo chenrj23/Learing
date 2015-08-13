88a6d3fdd6ab7007841f475c7454b577b2bc8e1d
陈人杰 <502111298@qq.com>
2015-08-14 01:05:48 +0800

    update
    。。。。感觉用了最笨的办法

--- python/编程导论/第二章/输出三个数中的最大奇数.py
+++ python/编程导论/第二章/输出三个数中的最大奇数.py
@@ -1,7 +1,32 @@
-x, y, z = 1, 2, 3
-if z > y and z > x and z % 2 != 0
-    print z
-elif x > y and x > z and x % 2 != 0
-    print x
-else y > x and y¡¡> z and y % 2 != 0
-    print y
+# -*- coding: cp936 -*-
+x, y, z = 1, 2, 4
+if x%2 != 0  and y%2 != 0 and z%2 !=0:
+    if x > y and x > z:
+        print 'x is biggest odd'
+    elif y > z:
+        print 'y is biggest odd'
+    else:
+        print 'z is biggest odd'
+elif x%2 != 0 and y%2 == 0 and z%2 ==0:
+    print 'x is biggest odd'
+elif y%2 != 0 and x%2 == 0 and z%2 ==0:
+    print 'y is biggest odd'
+elif z%2 != 0 and y%2 == 0 and x%2 ==0:
+    print 'z is biggest odd'
+elif x%2 != 0 and y%2 != 0 and z%2 ==0:
+    if x > y :
+        print 'x is biggest odd'
+    else:
+        print 'y is biggest odd'
+elif x%2 != 0 and y%2 == 0 and z%2 !=0:
+    if x > z :
+        print 'x is biggest odd'
+    else:
+        print 'z is biggest odd'
+elif x%2 == 0 and y%2 != 0 and z%2 !=0:
+    if y > z :
+        print 'y is biggest odd'
+    else:
+        print 'z is biggest odd'
+else:
+    print "no odd number"
